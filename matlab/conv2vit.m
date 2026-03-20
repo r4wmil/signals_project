58d97a7359a9266df3286ba902146015f3f76270
Ramil Khasanshin <ramil345655@gmail.com>
2026-03-20 12:52:39 +0300

    refactor(conv57_tests): separate simulate function

--- matlab/conv2vit.m
+++ matlab/conv2vit.m
@@ -1,17 +1,25 @@
-
+% Input values
 N = 1000;
 noise_mse = 0.3;
 trel = poly2trellis(3, [5 7]);
 traceback = 5 * 3;
 
+% Simulating
 data = randi([0, 1], N, 1);
-data_noise = data + noise_mse * randn(length(data), 1);
-data_noise = max(0, min(1, round(data_noise)))
+[erru, errc] = sim_conv2vit(data, trel, traceback, 0.3)
 
-data_conv = convenc(data, trel);
-data_conv_noise = data_conv + noise_mse * randn(length(data_conv), 1);
-data_conv_noise = max(0, min(1, round(data_conv_noise)))
-data_conv_dec = vitdec(data_conv_noise, trel, traceback, 'trunc', 'hard');
-
-err = mean(data ~= data_noise)
-err_conv = mean(data ~= data_conv_dec)
+function [err_unc, err_cod] = sim_conv2vit(data, trel, traceback, mse)
+    % Uncoded
+    data_noise = data + mse * randn(length(data), 1);
+    data_noise = round(data_noise);
+    data_noise = max(0, min(1, data_noise));
+    err_unc = mean(data ~= data_noise);
+    
+    % Coded
+    data_conv = convenc(data, trel);
+    data_conv_noise = data_conv + mse * randn(length(data_conv), 1);
+    data_conv_noise = round(data_conv_noise);
+    data_conv_noise = max(0, min(1, data_conv_noise));
+    data_conv_dec = vitdec(data_conv_noise, trel, traceback, 'trunc', 'hard');
+    err_cod = mean(data ~= data_conv_dec);
+end
