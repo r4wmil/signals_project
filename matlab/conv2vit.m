df85e15b2963d4c5c52f8d7792be42984a5d261f
Ramil Khasanshin <ramil345655@gmail.com>
2026-03-20 13:16:32 +0300

    feat(conv57_tests): conv2vit error graph for uncoded & coded versions

--- matlab/conv2vit.m
+++ matlab/conv2vit.m
@@ -1,14 +1,31 @@
 % Input values
-N = 1000;
-noise_mse = 0.3;
+N = 20000;
+noise_mse = 0.1 : 0.05 : 0.8;
 trel = poly2trellis(3, [5 7]);
 traceback = 5 * 3;
 
 % Simulating
 data = randi([0, 1], N, 1);
-[erru, errc] = sim_conv2vit(data, trel, traceback, 0.3)
+err_unc = zeros(size(noise_mse));
+err_cod = zeros(size(noise_mse));
+for i = 1:length(noise_mse)
+    [err_unc(i), err_cod(i)] = sim_conv2vit(data, trel, traceback, noise_mse(i));
+end
 
-function [err_unc, err_cod] = sim_conv2vit(data, trel, traceback, mse)
+% Plotting
+figure;
+hold on;
+semilogy(noise_mse, err_unc, 'b-o', 'LineWidth', 2);
+semilogy(noise_mse, err_cod, 'r-s', 'LineWidth', 2);
+hold off;
+grid on;
+xlabel('MSE (Noise Power)');
+ylabel('Bit Error Rate');
+legend('Uncoded', 'Convolutional Coded');
+
+% Functions
+
+function [err_unc, err_cod] = sim_conv2vit(data, trel, tb, mse)
     % Uncoded
     data_noise = data + mse * randn(length(data), 1);
     data_noise = round(data_noise);
@@ -20,6 +37,6 @@ function [err_unc, err_cod] = sim_conv2vit(data, trel, traceback, mse)
     data_conv_noise = data_conv + mse * randn(length(data_conv), 1);
     data_conv_noise = round(data_conv_noise);
     data_conv_noise = max(0, min(1, data_conv_noise));
-    data_conv_dec = vitdec(data_conv_noise, trel, traceback, 'trunc', 'hard');
+    data_conv_dec = vitdec(data_conv_noise, trel, tb, 'trunc', 'hard');
     err_cod = mean(data ~= data_conv_dec);
 end
