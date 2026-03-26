66b1ee3332a77f5f810f1c881088ca95edc45dee
Ramil Khasanshin <ramil345655@gmail.com>
2026-03-26 22:06:58 +0300

    feat: graph of the bit error rate depending on EbNo

--- matlab/conv2vit.m
+++ matlab/conv2vit.m
@@ -1,42 +1,54 @@
 % Input values
-N = 20000;
-noise_mse = 0.1 : 0.05 : 0.8;
+N = 10000;
+EbNo = 0:1:12; % Energy/bit to noise power
+%TODO (later): trials
 trel = poly2trellis(3, [5 7]);
 traceback = 5 * 3;
 
 % Simulating
 data = randi([0, 1], N, 1);
-err_unc = zeros(size(noise_mse));
-err_cod = zeros(size(noise_mse));
-for i = 1:length(noise_mse)
-    [err_unc(i), err_cod(i)] = sim_conv2vit(data, trel, traceback, noise_mse(i));
+err_unc = zeros(size(EbNo));
+err_cod = zeros(size(EbNo));
+for i = 1:length(EbNo)
+    [err_unc(i), err_cod(i)] = sim_conv2vit(data, trel, traceback, EbNo(i));
 end
 
 % Plotting
 figure;
 hold on;
-semilogy(noise_mse, err_unc, 'b-o', 'LineWidth', 2);
-semilogy(noise_mse, err_cod, 'r-s', 'LineWidth', 2);
+semilogy(EbNo, err_unc, 'b-o', 'LineWidth', 2);
+semilogy(EbNo, err_cod, 'r-s', 'LineWidth', 2);
 hold off;
 grid on;
-xlabel('MSE (Noise Power)');
+xlabel('Eb/No');
 ylabel('Bit Error Rate');
 legend('Uncoded', 'Convolutional Coded');
 
 % Functions
 
-function [err_unc, err_cod] = sim_conv2vit(data, trel, tb, mse)
+function [err_unc, err_cod] = sim_conv2vit(data, trel, tb, EbNo)
     % Uncoded
-    data_noise = data + mse * randn(length(data), 1);
+    snr = EbNo + 10 * log10(1); % Signal/Noise
+    snr = 10^(snr/10);
+    modulated = 2 * data - 1;
+    data_noise = awgn(modulated, snr);
     data_noise = round(data_noise);
-    data_noise = max(0, min(1, data_noise));
+    data_noise = max(-1, min(1, data_noise));
+    data_noise >= 0;
     err_unc = mean(data ~= data_noise);
     
     % Coded
+    k = log2(trel.numInputSymbols);
+    n = log2(trel.numOutputSymbols);
+    conv_rate = k / n;
+    snr = EbNo + 10 * log10(1 * conv_rate);
+    snr = 10^(snr/10);
     data_conv = convenc(data, trel);
-    data_conv_noise = data_conv + mse * randn(length(data_conv), 1);
+    modulated = 2 * data_conv - 1;
+    data_conv_noise = awgn(modulated, snr);
     data_conv_noise = round(data_conv_noise);
-    data_conv_noise = max(0, min(1, data_conv_noise));
+    data_conv_noise = max(-1, min(1, data_conv_noise));
+    data_conv_noise = data_conv_noise >= 0;
     data_conv_dec = vitdec(data_conv_noise, trel, tb, 'trunc', 'hard');
     err_cod = mean(data ~= data_conv_dec);
 end
