e13b85af7c1c28a7a0a3bb0f23cac056d46bcad2
Ramil Khasanshin <khasanshin.ramil@list.ru>
2026-04-18 15:16:41 +0300

    feat(conv57): decoding assuming no errors

--- matlab/conv57/conv57.h
+++ matlab/conv57/conv57.h
@@ -2,9 +2,18 @@
 #define CONV57_H_
 
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 
 #define ENC_LEN(_l) (((_l) - 2) * 2)
+
+#define LOG_BINARY(arr_, len_) \
+	do { \
+		for (size_t i = 0; i < (len_); i++) { \
+			printf("%d", (arr_)[i]); \
+		} \
+		printf("\n"); \
+	} while(0)
 
 #define LOG_COMPLEX(real_, imag_, len_) \
 	do { \
@@ -15,6 +24,7 @@
 	} while(0)
 
 void enc(bool* d, size_t dl, size_t ol, double* or, double* oi);
+bool dec_no_err(double* dr, double* di, size_t dl, bool* o);
 
 #endif /* CONV57_H_ */
 
@@ -31,5 +41,23 @@ void enc(bool* d, size_t dl, size_t ol, double* or, double* oi) {
 	}
 }
 
+bool dec_no_err(double* dr, double* di, size_t dl, bool* o) {
+	uint32_t prev = 0;
+	uint32_t w = 0;
+	size_t j = 0;
+	for (size_t i = 0; i < dl; i += 2) {
+		uint32_t curr = ((dr[i + 0] > 0) << 1) | (dr[i + 1] > 0);
+		uint32_t w0 = ((w << 1) | 0x0) & 0x7;
+		uint32_t w1 = ((w << 1) | 0x1) & 0x7;
+		uint32_t c0 = (__builtin_parity(w0 & 0x5) % 2 << 1) | __builtin_parity(w0 & 0x7);
+		uint32_t c1 = (__builtin_parity(w1 & 0x5)  << 1) | __builtin_parity(w1 & 0x7);
+		if (curr == c0) { w = w0; }
+		else if (curr == c1) { w = w1; }
+		else { return false; }
+		o[j++] = (w >> 2) & 0x1;
+		prev = curr;
+	}
+	return true;
+}
 
 #endif /* CONV57_IMPLEMENTATION */
--- matlab/conv57/conv57main.c
+++ matlab/conv57/conv57main.c
@@ -2,10 +2,16 @@
 
 int main() {
 	bool inp[] = {0,0,0,1,1,0,1,0,0,0};
+	bool out[] = {0,0,0,0,0,0,0,0,0,0};
 	size_t inp_len = sizeof(inp)/sizeof(*inp);
-	const size_t out_len = ENC_LEN(inp_len);
-	double out_real[out_len];
-	double out_imag[out_len];
-	enc(inp, inp_len, out_len, out_real, out_imag);
-	LOG_COMPLEX(out_real, out_imag, out_len);
+	size_t out_len = sizeof(out)/sizeof(*out);
+	LOG_BINARY(inp, inp_len);
+	const size_t enc_len = ENC_LEN(inp_len);
+	double enc_real[enc_len];
+	double enc_imag[enc_len];
+	enc(inp, inp_len, enc_len, enc_real, enc_imag);
+	//enc_real[3] = 0;
+	LOG_COMPLEX(enc_real, enc_imag, enc_len);
+	printf("ok=%d\n", dec_no_err(enc_real, enc_imag, enc_len, out));
+	LOG_BINARY(out, out_len);
 }
