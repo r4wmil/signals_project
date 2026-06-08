a14fc6b1423c5472c55eef18fb6975c78dddb3c2
Ramil Khasanshin <ramil345655@gmail.com>
2026-06-09 02:40:06 +0300

    chore: file for experimentation

---
+++ matlab_check.m
@@ -0,0 +1,13 @@
+function enc = check57(data)
+    trel = poly2trellis(3, [5 7]);
+    enc = convenc(data, trel);
+end
+
+check57([1,1]);
+
+function enc = check2d(data)
+    trel = poly2trellis([3 3], [5 7; 1 4]);
+    enc = convenc(data, trel);
+end
+
+check2d([1 1 1 1])
