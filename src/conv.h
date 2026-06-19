b1d0423a6424af56ab12b81f62828dc61cc5ae9d
Ramil Khasanshin <ramil345655@gmail.com>
2026-06-20 01:11:56 +0300

    chore(mconv_c): first trel test

--- src/conv.h
+++ src/conv.h
@@ -1,8 +1,35 @@
 #ifndef CONV_H_
 #define CONV_H_
 
+#include "stdint.h"
+#include "stdlib.h"
+
+typedef struct conv_trel_t {
+	size_t c;   // constraint length
+	size_t k;   // input bits
+	size_t n;   // output bits
+	size_t is;  // number of input symbols
+	size_t os;  // number of output symbols
+	size_t s;   // number of states
+	uint64_t* N; // next states
+	uint64_t* O; // outputs
+} conv_trel_t;
+
 #endif /* CONV_H_ */
 
 #ifdef CONV_IMPLEMENTATION
+
+conv_trel_t conv_trel_gen(size_t c, uint32_t* gen, size_t len) {
+	conv_trel_t t = {0};
+	t.c = c;
+	t.k = 1;
+	t.n = len;
+	t.is = 0x1 << t.k;
+	t.os = 0x1 << t.n;
+	t.s = 0x1 << ((c - 1) * t.k);
+	t.N = malloc(t.s * (0x1 << t.k));
+	t.O = malloc(t.s * (0x1 << t.k));
+	return t;
+}
 
 #endif /* CONV_IMPLEMENTATION */
---
+++ src/tests.c
@@ -0,0 +1,42 @@
+#define CONV_IMPLEMENTATION
+#include "conv.h"
+#undef CONV_IMPLEMENTATION
+
+#include "stdio.h"
+#include "stdint.h"
+#include "assert.h"
+
+//size_t c;   // constraint length
+//size_t k;   // input bits
+//size_t n;   // output bits
+//size_t is;  // number of input symbols
+//size_t os;  // number of output symbols
+//size_t s;   // number of states
+//uint64_t N; // next states
+//uint64_t O; // outputs
+
+void ut_trel(size_t c, uint32_t* gen, size_t len,
+	size_t k, size_t n,
+	size_t is, size_t os, size_t s,
+	uint64_t* N,
+	uint64_t* O) {
+	conv_trel_t t = conv_trel_gen(c, gen, len);
+	assert(t.c == c);
+	assert(t.k == k);
+	assert(t.is == is);
+	assert(t.os == os);
+	assert(t.s == s);
+	assert(t.N);
+	assert(t.O);
+	for (size_t i = 0; i < t.s * t.is; i++) { assert(N[i] == t.N[i]); }
+	for (size_t i = 0; i < t.s * t.is; i++) { assert(O[i] == t.O[i]); }
+}
+
+int main(int argc, char** argv) {
+	ut_trel(3, (uint32_t[]){0x5,0x7}, 2,
+		1, 2,
+		2, 4, 4,
+		(uint64_t[]){0,2,0,2,1,3,1,3},
+		(uint64_t[]){0,3,3,0,1,2,2,1});
+	return 0;
+}
