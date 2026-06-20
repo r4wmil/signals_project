cd4a97aa8c59f91d3eeb1ba3b62472b97d4e8637
Ramil Khasanshin <ramil345655@gmail.com>
2026-06-20 15:33:20 +0300

    chore(mconv_c): trellis next_states array generation

--- src/conv.h
+++ src/conv.h
@@ -3,6 +3,7 @@
 
 #include "stdint.h"
 #include "stdlib.h"
+#include "stdio.h"
 
 typedef struct conv_trel_t {
 	size_t c;   // constraint length
@@ -19,16 +20,25 @@ typedef struct conv_trel_t {
 
 #ifdef CONV_IMPLEMENTATION
 
-conv_trel_t conv_trel_gen(size_t c, uint32_t* gen, size_t len) {
+conv_trel_t conv_trel_gen(size_t c, uint32_t* G, size_t n) {
 	conv_trel_t t = {0};
 	t.c = c;
-	t.k = 1;
-	t.n = len;
+	t.k = 1; // TODO: 2D
+	t.n = n;
 	t.is = 0x1 << t.k;
 	t.os = 0x1 << t.n;
-	t.s = 0x1 << ((c - 1) * t.k);
-	t.N = malloc(t.s * (0x1 << t.k));
-	t.O = malloc(t.s * (0x1 << t.k));
+	t.s = 0x1 << ((t.c - 1) * t.k);
+	t.N = malloc(t.s * t.is * sizeof(*t.N));
+	t.O = malloc(t.s * t.is * sizeof(*t.O));
+	for (size_t s = 0; s < t.s; s++) {
+		for (size_t i = 0; i < t.is; i++) {
+			t.N[s * t.is + i] = (s >> t.k) | (i << ((t.c - 2) * t.k));
+			t.O[s * t.is + i] = 0;
+			//for (size_t g = 0; g < len; g++) {
+			//	t.O[s * t.is + i] = G[g];
+			//}
+		}
+	}
 	return t;
 }
 
